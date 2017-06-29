964ee8ac79244c12e7a0ea7bfcdad3b12494c451
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-06-29 17:39:54 -0400

    Add TimelineLite code to animate cup zoom

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 var tl = new TimelineLite();
 
-tl.to(".display", 10, {backgroundPosition:"47% 60%"});
-// .to(".display", 2, {backgroundSize: "225%"}, "-=3");
+tl.to(".display", 3.5, {backgroundPosition:"47% 60%",ease: Power0.easeNone})
+.to(".display", 3.5, {backgroundSize:"225%",ease: Power3.easeIn}, "-=4")
+.to(".display", 3.5, {backgroundPosition:"70% 100%", ease: Power0.easeNone}, "zoomout")
+.to(".display", 3.5, {backgroundSize:"175%", ease: Power0.easeNone}, "zoomout")
+.to(".display", 4, {backgroundSize:"160%", ease: Power0.easeNone}, "zoomout-2")
+.to(".display", 4, {backgroundPosition:"5% 125%"}, "zoomout-2");
